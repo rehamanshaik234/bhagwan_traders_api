ecfacc4f277e36d558968d352cc11c0ee37793fa
Rehaman Shaik <srehaman234@gmail.com>
2025-07-31 10:56:43 +0530

    fix: update file upload destinations to correct path for image storage

--- helpers/dashboard/fileupload.js
+++ helpers/dashboard/fileupload.js
@@ -3,28 +3,28 @@ const path = require("path");
 
 // Go outside the project folder
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../uploads"), // Go one folder up
+  destination: path.join(__dirname, "../../../uploads"), // Go one folder up
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
 const categoryImagesStorage = multer.diskStorage({
-  destination: path.join(__dirname, "../uploads/category"), // Go one folder up
+  destination: path.join(__dirname, "../../../uploads/category"), // Go one folder up
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
 const subCategoryImagesStorage = multer.diskStorage({
-  destination: path.join(__dirname, "../uploads/subcategory"), // Go one folder up
+  destination: path.join(__dirname, "../../../uploads/subcategory"), // Go one folder up
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
 const productImagesStorage = multer.diskStorage({
-  destination: path.join(__dirname, "../uploads/product"), // Go one folder up
+  destination: path.join(__dirname, "../../../uploads/product"), // Go one folder up
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   },
