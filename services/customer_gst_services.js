8ebcea52d6ee9109f04b6507e477891a61cc3f0d
Rehaman Shaik <srehaman234@gmail.com>
2025-08-04 12:25:30 +0530

    fix: add getCustomerGST function to retrieve GST by GST number

--- services/customer_gst_services.js
+++ services/customer_gst_services.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const fndb = require("../helpers/dbFunctions.js");
 const { AddressCols, CustomerGstCols } = require("../helpers/tableColumns.js");
 const authenticateToken = require("../helpers/authtoken");
+const { get } = require("http");
 
 router.post("/addGst", [authenticateToken.validJWTNeeded, addGst]);
 router.get("/getCustomerGSTS", [authenticateToken.validJWTNeeded, getCustomerGSTS]);
+router.get("/getCustomerGST", [authenticateToken.validJWTNeeded, getCustomerGST]);
 
 
 async function addGst(req, res) {
@@ -35,6 +37,26 @@ async function getCustomerGSTS(req, res) {
   try {
     var customer_id = req.query.customer_id;
     const result = await fndb.getAllItemsByID(tableNames.customer_gsts, CustomerGstCols.customer_id, customer_id);
+    if (result != null) {
+      resp = {
+        status: true,
+        message: `GST Retrieved Successfully`,
+        data: result,
+      };
+    } else {
+      resp = { status: false, error: "Query execution error" };
+    }
+  } catch (error) {
+    resp = { status: false, error: error };
+  }
+  return res.send(resp);
+}
+
+async function getCustomerGST(req, res) {
+  var resp = new Object();
+  try {
+    var gst_number = req.query.gst_number;
+    const result = await fndb.getItemByColumn(tableNames.customer_gsts, CustomerGstCols.gst_number, gst_number);
     if (result != null) {
       resp = {
         status: true,
